2af0ef7ba45929ba9f718b3b50b840a2e8b2f4b5
Davide De Rosa <keeshux@gmail.com>
2015-10-18 20:39:52 +0200

    Prefer readability over "speed"

--- common.h
+++ common.h
@@ -6,14 +6,16 @@
 #include <stdint.h>
 #include "endian.h"
 
+size_t bbp_eoffset(size_t i, size_t len) {
+    return ((bbp_host_endian() == BBP_LITTLE) ? i : (len - i - 1));
+}
+
 void bbp_print_hex(const char *label, const uint8_t *v, size_t len) {
     size_t i;
-    const size_t mul = ((bbp_host_endian() == BBP_LITTLE) ? 0 : 1);
 
     printf("%s: ", label);
     for (i = 0; i < len; ++i) {
-        const size_t offset = mul * (len - 2 * i - 1);
-        printf("%02x", v[i + offset]);
+        printf("%02x", v[bbp_eoffset(i, len)]);
     }
     printf("\n");
 }
@@ -30,21 +32,18 @@ uint8_t bbp_hex2byte(const char ch) {
 
 void bbp_parse_hex(bbp_endian_t e, uint8_t *v, const char *str) {
     const size_t count = strlen(str) / 2;
-    const size_t mul = ((e == BBP_LITTLE) ? 0 : 1);
     size_t i;
 
     for (i = 0; i < count; ++i) {
         const char hi = bbp_hex2byte(str[i * 2]);
         const char lo = bbp_hex2byte(str[i * 2 + 1]);
-        const size_t offset = mul * (count - 2 * i - 1);
 
-        v[i + offset] = hi * 16 + lo;
+        v[bbp_eoffset(i, count)] = hi * 16 + lo;
     }
 }
 
 uint8_t *bbp_alloc_hex(bbp_endian_t e, const char *str, size_t *len) {
     const size_t count = strlen(str) / 2;
-    const size_t mul = ((e == BBP_LITTLE) ? 0 : 1);
     size_t i;
 
     uint8_t *v = malloc(count);
@@ -52,9 +51,8 @@ uint8_t *bbp_alloc_hex(bbp_endian_t e, const char *str, size_t *len) {
     for (i = 0; i < count; ++i) {
         const char hi = bbp_hex2byte(str[i * 2]);
         const char lo = bbp_hex2byte(str[i * 2 + 1]);
-        const size_t offset = mul * (count - 2 * i - 1);
 
-        v[i + offset] = hi * 16 + lo;
+        v[bbp_eoffset(i, count)] = hi * 16 + lo;
     }
 
     *len = count;
