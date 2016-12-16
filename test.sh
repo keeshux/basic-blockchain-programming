2ec8f2ff3dc902d5585f8b5c8bae5b8bed992b49
Davide De Rosa <keeshux@gmail.com>
2016-12-16 17:27:49 +0100

    Allow long long

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 OPENSSL_INCLUDE=/usr/local/opt/openssl/include/
 touch $1.c
-gcc -ansi -Wall -Wno-deprecated-declarations -I $OPENSSL_INCLUDE -pedantic -lcrypto -o $1.out $1.c
+gcc -ansi -Wall -Wno-deprecated-declarations -Wno-long-long -I $OPENSSL_INCLUDE -pedantic -lcrypto -o $1.out $1.c
 ./$1.out
